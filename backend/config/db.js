b67af6ef3bfd17b8ed0e45cd2d6c6fb98a4905c4
GunaSekhar <kudimigunasekhar@gmail.com>
2025-10-22 17:39:27 +0530

    checking

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   user: process.env.PGUSER || 'postgres',
   host: process.env.PGHOST || 'localhost',
   database: process.env.PGDATABASE || 'waste_bin_db',
-  password: process.env.PGPASSWORD || '1234567890',
+  password: process.env.PGPASSWORD || 'aadhi',
   port: process.env.PGPORT ? Number(process.env.PGPORT) : 5432,
 });
 
